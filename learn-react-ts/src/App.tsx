1fa49df209ea6c76d2dece218802d1db94fde6f1
Huy ne <bmhuyquoc104@gmail.com>
2022-05-22 17:30:15 +0700

    7. Learn object type, arr of object type in ts

--- learn-react-ts/src/App.tsx
+++ learn-react-ts/src/App.tsx
@@ -1,8 +1,32 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Profile from "./components/Profile";
+
 function App() {
+  const address = {
+    street: "Nguyen Tuan Street",
+    city: "Sai Gon",
+  }
+  const skills = [
+    {
+      frontend:"React",
+      backend:"NodeJS",
+      id: 1,
+    },
+    {
+      frontend:"React",
+      backend:"Spring",
+      id: 2,
+    },
+    {
+      frontend:"PHP",
+      backend:"Lavarel",
+      id: 3,
+    },
+  ]
   return <div className="App">
-    <Header name = "huy" age = {21}/>
+    {/* <Header name = "huy" age = {21}/> */}
+    <Profile name="huy" age={21} isLoggedIn={true} address={address} skills={skills} />
   </div>;
 }
 
---
+++ learn-react-ts/src/components/Profile.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+type ProfileProps = {
+  name: string;
+  age: number;
+  isLoggedIn: boolean;
+  skills: {
+    id: number | string;
+    frontend: string;
+    backend: string;
+  }[];
+  address: {
+    street: string;
+    city: string;
+  };
+};
+
+function Profile({ name, age, skills, address, isLoggedIn }: ProfileProps) {
+  return (
+    <div>
+      {isLoggedIn ? (
+        <div>
+          <h1>Welcome {name} </h1>
+          <h2>Age:{age}</h2>
+          {skills.map((skill) => (
+            <ul key={skill.id}>
+              <li>
+                Skill: {skill.frontend} {skill.backend}
+              </li>
+            </ul>
+          ))}
+          <h2>
+            Address: {address.street}, {address.city}{" "}
+          </h2>
+        </div>
+      ) : (
+        <h1> Welcome guest</h1>
+      )}
+    </div>
+  );
+}
+
+export default Profile;
