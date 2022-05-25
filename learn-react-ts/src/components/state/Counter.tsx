3d6fc1b9f2600553aeac30e759398746f8104df3
huy vo <bmhuyquoc104@gmail.com>
2022-05-25 11:36:03 +0700

    13. Rewrite reducer in typesciprt

--- learn-react-ts/src/components/state/Counter.tsx
+++ learn-react-ts/src/components/state/Counter.tsx
@@ -1,16 +1,32 @@
 import React, { useReducer } from "react";
 
+type CounterType = {
+  count: number;
+};
+
+type UpdateType = {
+  type: "decrement" | "increment";
+  payload: number;
+};
+
+type ResetType = {
+  type: "reset";
+};
+
 function Counter() {
   const initialValue = {
     count: 0,
   };
-  const reducer = (state: any, action: any) => {
+  const reducer = (state: CounterType, action: UpdateType | ResetType) => {
     switch (action.type) {
       case "increment": {
         return { count: state.count + action.payload };
       }
       case "decrement": {
         return { count: state.count - action.payload };
+      }
+      case "reset": {
+        return initialValue;
       }
       default:
         return state;
@@ -28,6 +44,7 @@ function Counter() {
       <button onClick={() => dispatch({ type: "decrement", payload: 1 })}>
         Decrease
       </button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </div>
   );
 }
